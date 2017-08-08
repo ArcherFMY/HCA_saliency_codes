0501dee99f7e6cb25f28720c66a5da125fd683f4
ArcherFMY <Mengyang_Feng@mail.dlut.edu.cn>
2017-08-08 15:00:16 +0800

    Update executeUpdate.m

--- func/CCA/executeUpdate.m
+++ func/CCA/executeUpdate.m
@@ -3,5 +3,5 @@ function Updated = executeUpdate(S_M,threshold,v,index)
     Sigma.o             = sign(S_M - threshold(:,1:size(S_M,2)));
     Sigma.all           = sign(S_M(index)- threshold);
 
-    Updated             = S_M + (repmat(sum(Sigma.o,2) + sum(Sigma.all,2),[1,3]) - Sigma.o)*v;
+    Updated             = S_M + (repmat(sum(Sigma.o,2) + sum(Sigma.all,2),[1,size(S_M, 2)]) - Sigma.o)*v;
 
